cf35bde565c88109b62febfc281298a8f99d2c74
Sovichea168Cambo <phatsovichea@gmail.com>
2025-10-24 08:47:52 +0700

    G'day mate

---
+++ vichea.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    system("cls");
+
+    int A[5] = {69, 168, 89, 77, 52};
+
+    for(int i = 0; i < 5; i++){
+        cout<<A[i]<<endl;
+    }
+
+    return 0;
+}
